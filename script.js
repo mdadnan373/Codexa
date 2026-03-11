4f793e84a9eabd762f5e3de17022aba0020be417
mdadnan373 <rsadnan333@gmail.com>
2026-03-12 00:12:29 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-const cards = document.querySelectorAll(".card");
+const canvas = document.getElementById("bg");
+const ctx = canvas.getContext("2d");
 
-window.addEventListener("scroll",()=>{
-cards.forEach(card=>{
-const top = card.getBoundingClientRect().top;
-if(top < window.innerHeight - 100){
-card.style.opacity="1";
-card.style.transform="translateY(0)";
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particles = [];
+
+for(let i=0;i<80;i++){
+particles.push({
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+vx:(Math.random()-0.5)*1,
+vy:(Math.random()-0.5)*1
+});
 }
+
+function animate(){
+
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+particles.forEach(p=>{
+
+p.x += p.vx;
+p.y += p.vy;
+
+if(p.x<0||p.x>canvas.width)p.vx*=-1;
+if(p.y<0||p.y>canvas.height)p.vy*=-1;
+
+ctx.beginPath();
+ctx.arc(p.x,p.y,2,0,Math.PI*2);
+ctx.fillStyle="#00d9ff";
+ctx.fill();
+
 });
-});
+
+requestAnimationFrame(animate);
+}
+
+animate();
+
